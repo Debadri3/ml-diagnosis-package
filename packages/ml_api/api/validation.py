06b65d46d5951f07adf5622adf906803c9b5ddd2
Debadri <debadri3@gmail.com>
2021-09-18 09:19:49 +0530

    initial commit

---
+++ packages/ml_api/api/validation.py
@@ -0,0 +1,78 @@
+import typing as t
+
+from marshmallow import Schema, fields
+from marshmallow import ValidationError
+
+from api import config
+
+
+class InvalidInputError(Exception):
+    """Invalid model input."""
+
+
+SYNTAX_ERROR_FIELD_MAP = {
+    '1stFlrSF': 'FirstFlrSF',
+    '2ndFlrSF': 'SecondFlrSF',
+    '3SsnPorch': 'ThreeSsnPortch'
+}
+
+
+class CancerDataRequestSchema(Schema):
+    Gene=fields.Str()
+    Variation=fields.Str()
+    Text=fields.Str()
+
+
+def _filter_error_rows(errors: dict,
+                       validated_input: t.List[dict]
+                       ) -> t.List[dict]:
+    """Remove input data rows with errors."""
+
+    indexes = errors.keys()
+    # delete them in reverse order so that you
+    # don't throw off the subsequent indexes.
+    for index in sorted(indexes, reverse=True):
+        del validated_input[index]
+
+    return validated_input
+
+
+def validate_inputs(input_data):
+    """Check prediction inputs against schema."""
+
+    # set many=True to allow passing in a list
+    schema = CancerDataRequestSchema(strict=True, many=True)
+
+    # convert syntax error field names (beginning with numbers)
+    for dict in input_data:
+        for key, value in SYNTAX_ERROR_FIELD_MAP.items():
+            dict[value] = dict[key]
+            del dict[key]
+
+    errors = None
+    try:
+        schema.load(input_data)
+    except ValidationError as exc:
+        errors = exc.messages
+
+    # convert syntax error field names back
+    # this is a hack - never name your data
+    # fields with numbers as the first letter.
+    for dict in input_data:
+        for key, value in SYNTAX_ERROR_FIELD_MAP.items():
+            dict[key] = dict[value]
+            del dict[value]
+
+    if errors:
+        validated_input = _filter_error_rows(
+            errors=errors,
+            validated_input=input_data)
+    else:
+        validated_input = input_data
+
+    return validated_input, errors
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in config.ALLOWED_EXTENSIONS
---
+++ scripts/fetch_kaggle_dataset.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+kaggle competitions download -c msk-redefining-cancer-treatment -p packages/regression_model/regression_model/datasets/
